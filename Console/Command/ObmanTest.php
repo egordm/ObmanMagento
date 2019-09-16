7c82c9b5ff66509f59cb46e3c51bfe944370e78f
Egor Dmitriev <egordmitriev2@gmail.com>
2019-09-16 23:53:53 +0200

    Added some more classes for testing

--- Console/Command/ObmanTest.php
+++ Console/Command/ObmanTest.php
@@ -5,7 +5,9 @@ namespace EgorDm\Obman\Console\Command;
 
 use EgorDm\Obman\Wrappers\ObmanFactory;
 use EgorDm\Obman\Wrappers\ObmanObjectManager;
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\ObjectManager\ConfigInterface;
+use Magento\Quote\Model\Quote;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -43,7 +45,27 @@ class ObmanTest extends Command
         InputInterface $input,
         OutputInterface $output
     ) {
-        $output->writeln("Hello World");
+        $om = ObjectManager::getInstance();
+
+        $start = microtime(true);
+        $data = $om->create(\Magento\Sales\Model\Order::class);
+        $data = $om->create(\Magento\Quote\Model\Quote::class);
+        $data = $om->create(\Magento\Catalog\Block\Adminhtml\Form::class);
+        $data = $om->create(\Magento\Catalog\Block\Adminhtml\Product::class);
+        $data = $om->create(\Magento\Checkout\Block\Cart::class);
+        $data = $om->create(\Magento\Checkout\Block\Registration::class);
+        $data = $om->create(\Magento\Swagger\Block\Index::class);
+        $data = $om->create(\Magento\Tax\Block\Checkout\Tax::class);
+        $data = $om->create(\Magento\Theme\Block\Html\Header::class);
+        $data = $om->create(\Magento\Wishlist\Block\Customer\Wishlist::class);
+        $data = $om->create(\Magento\Customer\Helper\View::class);
+        $data = $om->create(\Magento\Customer\Helper\View::class);
+        $data = $om->create(\Magento\Sales\Helper\Data::class);
+        $data = $om->create(\Magento\Catalog\Helper\Data::class);
+        $data = $om->create(\Magento\Catalog\Helper\Product::class);
+        $end = microtime(true);
+
+        $output->writeln("Spent " . ($end - $start) . "s loading dependency.");
     }
 
     /**
--- Constants.php
+++ Constants.php
@@ -6,6 +6,6 @@ namespace EgorDm\Obman;
 
 class Constants
 {
-    const ENABLE_OBMAN = false;
-//    const ENABLE_OBMAN = true;
+//    const ENABLE_OBMAN = false;
+    const ENABLE_OBMAN = true;
 }
